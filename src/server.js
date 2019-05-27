d119d2e910fa4c1edf60c2a312b33b9a214c81ba
Luzimar Oliveira <lzm.spfc@gmail.com>
2019-05-27 14:44:09 -0400

    Update server.js
    adicionado rota para o arquivo

--- src/server.js
+++ src/server.js
@@ -37,12 +37,13 @@ class App {
       express: this.express,
       autoescape: true
     })
-
+    
     this.express.use(express.static(path.resolve(__dirname, 'public')))
     this.express.set('view engine', 'njk')
   }
 
   routes () {
+    this.express.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
     this.express.use(require('./routes'))
   }
 }
