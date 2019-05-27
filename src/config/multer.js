d3648dba16ed72601e95d4639c651cd73c70056c
Luzimar Oliveira <lzm.spfc@gmail.com>
2019-05-27 13:38:01 -0400

    Update multer.js
    Alteração na configuração do multer

--- src/config/multer.js
+++ src/config/multer.js
@@ -7,8 +7,11 @@ module.exports = {
     destination: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
     filename: (req, file, cb) => {
       crypto.randomBytes(16, (err, raw) => {
-        if (err) return cb(err)
-        cb(null, raw.toString('hex') + path.extname(file.originalname))
+        if(err) cb(err);
+
+                file.key = `${hash.toString('hex')}-${file.originalname}`;
+
+                cb(null, file.key);
       })
     }
   })
