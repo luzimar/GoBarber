ff4d725e5b3fd2f11906882f6b938ceabde03303
Luzimar Oliveira <lzm.spfc@gmail.com>
2019-05-27 13:50:25 -0400

    Update User.js
    Add url para avatar.

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -31,5 +31,9 @@ User.methods = {
     return bcrypt.compare(password, this.password)
   }
 }
-
+User.virtual("url").get(function(){
+    const url = process.env.URL || 'http://localhost:3333';
+    
+    return `${url}/files/${encodeURIComponent(this.avatar)}`;
+})
 module.exports = mongoose.model('User', User)
