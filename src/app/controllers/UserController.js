3c732c93d405f6fd6bbd8ee4ca35500f9db51a84
Luzimar Oliveira <lzm.spfc@gmail.com>
2019-05-28 13:31:09 -0400

    Update UserController.js

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -6,7 +6,7 @@ class UserController {
   }
 
   async store (req, res) {
-   const { avatar: key } = req.file
+    const { filename: avatar } = req.file
     if (req.body.provider === '1') {
       await Provider.create({ ...req.body, avatar })
     } else {
