c251e6c919d03fcb605f21a23fafd40993ba3c47
Luzimar Oliveira <lzm.spfc@gmail.com>
2019-05-28 23:03:38 -0300

    Update DashboardController.js

--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -2,8 +2,10 @@ const { Provider } = require('../models')
 
 class DashboardController {
   async index (req, res) {
+    const user = req.session.user
+    return res.render('dashboardProvider', { user })
     const providers = await Provider.find({})
-    return res.render('dashboard', { providers })
+    return res.render('dashboard', { user, providers })
   }
 }
 
